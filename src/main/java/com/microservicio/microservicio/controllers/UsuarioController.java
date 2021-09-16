bc77be61742e568f7e559060f435de475d1312c7
David Fernandez Ramirez <david.fernandez@soltel.es>
2021-09-16 23:25:56 +0200

    Creo nuevos controladores, añado moqueo de la tabla usuarios

--- src/main/java/com/microservicio/microservicio/controllers/UsuarioController.java
+++ src/main/java/com/microservicio/microservicio/controllers/UsuarioController.java
@@ -1,14 +1,47 @@
 package com.microservicio.microservicio.controllers;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import models.Usuario;
 
 @RestController
 public class UsuarioController {
 
-	@RequestMapping(value = "prueba")
-	public String prueba() {
-		return "prueba";
+	@RequestMapping(value = "usuario/{id}")
+	public Usuario getUsuario(@PathVariable Long id) {
+		Usuario usuario = new Usuario();
+		usuario.setId(id);
+		usuario.setNombre("David");
+		usuario.setApellido("Fernandez");
+		usuario.setEmail("dferna40@gmail.com");
+		usuario.setTelefono("675430271");
+		usuario.setPassword("pass");
+		return usuario;
 	}
+	
+	@RequestMapping(value = "modificarUsuario")
+	public Usuario modificarUsuario() {
+		Usuario usuario = new Usuario();
+		usuario.setNombre("David");
+		usuario.setApellido("Fernandez");
+		usuario.setEmail("dferna40@gmail.com");
+		usuario.setTelefono("675430271");
+		usuario.setPassword("pass");
+		return usuario;
+	}
+	
+	@RequestMapping(value = "eliminarUsuario")
+	public Usuario eliminarUsuario() {
+		Usuario usuario = new Usuario();
+		usuario.setNombre("David");
+		usuario.setApellido("Fernandez");
+		usuario.setEmail("dferna40@gmail.com");
+		usuario.setTelefono("675430271");
+		usuario.setPassword("pass");
+		return usuario;
+	}
+	
 	
 }
